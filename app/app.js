3bef7bf623ea7b3c6fd8bc927361ba2a98e3b8e9
Ignatius Seto <ignatius.seto@accenture.com>
2023-02-22 16:07:41 +0100

    feat(route-api): add function to show calculated results

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ const config = {
     ROUTE_ENDPOINT: `http://localhost:9000/.netlify/functions/route/`
   },
   element: {
-    CALCULATE_BTN: `#calculate-btn`
+    CALCULATE_BTN: `#calculate-btn`,
+    RESULT_LIST: `#result-list`
   }
 }
 
@@ -21,5 +22,22 @@ async function handleClick(event) {
     .then((response) => response.json())
     .then((data) => {
       fetch(config.endpoint.CARS_ENDPOINT + `${data.distance}/${data.time}`)
+        .then((response) => response.json())
+        .then(showResults)
     })
+}
+
+function showResults(data) {
+  const resultListEl = document.querySelector(config.element.RESULT_LIST)
+  document.querySelectorAll('li').forEach((el) => el.remove())
+
+  data.forEach((element) => {
+    console.log(element)
+    const listEl = document.createElement('li')
+    listEl.innerHTML = `<b>Anbieter:</b> ${element.firm} <br> <b>Modell:</b> ${
+      element.car
+    } <br> <b>Gesamtkosten: ${Math.round(element.total * 100) / 100} €</b>`
+
+    resultListEl.append(listEl)
+  })
 }
